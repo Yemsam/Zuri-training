b1fec456fd911eacb29e528acb6015eee4bf6007
Olayemi Olaoluwa Samuel <66535994+Yemsam@users.noreply.github.com>
2022-06-09 23:34:38 +0100

    Add files via upload
    This file contains 2 python codes:
    The first code is a function that can read text in a file 
    The second is a function that can count unique words in a file and store it as a dictionary.

---
+++ main.py
@@ -0,0 +1,17 @@
+def read_file_content(filename):
+    with open(filename) as file:
+        return file.read() # read it as a whole string
+print(read_file_content(r"...\hello.txt"))
+
+def count_words():
+    # calling the function and storing the output in `text`
+    text = read_file_content(r"...\story.txt")
+    output = {} # empty dictionary
+    # a set doesn't allow duplicates
+    unique = set(text.lower().split()) # convert to lowercase, then make all the words into a list
+    for word in unique: # for every word
+        output[word] = text.lower().split().count(word) # to make sure it's counting word by word, and not letter by letter
+    return output # {"as": 10, "would": 20}
+print(count_words())
+# driver
+# count_words()
